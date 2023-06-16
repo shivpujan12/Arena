49dab30c8c975c0580c517c03190b60e77e43ef7
Divyansh <divyanshgupta1106@gmail.com>
2023-06-16 14:35:10 +0530

    Integrated ChatBox Using socket.io

---
+++ iChat/nodeServer/index.js
@@ -0,0 +1,22 @@
+//Node server which will handle all Socket.io connections
+const io = require('socket.io')(process.env.PORT||8000,{cors : 'http://localhost:8000'});
+
+const users = {}
+
+
+io.on('connection',socket => {
+    socket.on('new-user-joined', name =>{
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined',name);
+    });
+    socket.on('send-message',message => {
+        socket.broadcast.emit('received', {message : message,name : users[socket.id]})
+    });
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('leave',users[socket.id]);
+        delete(users[socket.id]);
+    })
+});
+
+
+
